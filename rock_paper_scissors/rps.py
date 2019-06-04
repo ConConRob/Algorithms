cc20fecde5e5dee0937fa5c09c476517224e4647
conconrob <connortprobertson@gmail.com>
2019-06-04 18:41:55 +0100

    done rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,24 +1,48 @@
 #!/usr/bin/python
 
+import time
 import sys
-plays = ('rock', 'papper', 'scissors')
+possible_selections = ('rock', 'paper', 'scissors')
 
 
 def rock_paper_scissors(n):
     # template the arrays this
     # arrays = [[] for i in range(len(plays) ** n)]
-    return rock_paper_scissors_reducer(n, arrays=[])
+    if n == 0:
+        return [[]]
+    return rock_paper_scissors_reducer(n)
 
 
-def rock_paper_scissors_reducer(n, arrays, i=0):
+def rock_paper_scissors_reducer(n, i=2, lastplays=[[play] for play in possible_selections]):
     # base case
-    if i == n:
-        return arrays
-    return rock_paper_scissors_reducer(n, arrays, i + 1)
+    if i == n + 1:
+        return lastplays
+    # init case
+
+    plays = []
+    # number of arrays for this i
+    numarrs = 3**i
+    for j in range(numarrs):
+        # make plays that start with rocks for first 3rd
+        # make plays that start with papper for second 3rd
+        # make plays that start with scissors for last 3rd
+        # formual for this is j*3//numarrs
+        current_plays = [possible_selections[j*3//numarrs]]
+        # for past we need: current index - (number of loops * length last)
+        # last length = numsarr/3
+        # current loop of last array = j*3//numarrs
+        current_plays.extend(lastplays[int(j - numarrs//3 * (j*3//numarrs))])
+        plays.append(current_plays)
+    return rock_paper_scissors_reducer(n, i + 1, plays)
+
 
 # pattern number of arrays that start with same sol is 3^n/3 => 3^(n-1)
 #  the following plays in all the arrays are just the previous plays appened
 
+start = time.time()
+rock_paper_scissors(14)
+finish = time.time()
+print(finish-start)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
